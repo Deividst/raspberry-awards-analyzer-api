eabe0d8829810e6712dccd616e788214e574090b
deividst <deivid.thome@outlook.com>
2025-09-07 16:41:59 -0300

    feat:Criacao do componente

---
+++ src/main/java/com/github/deividst/api/RaspberryAwardsAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.deividst.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryAwardsAnalyzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryAwardsAnalyzerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/deividst/api/RaspberryAwardsAnalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deividst.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberryAwardsAnalyzerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
